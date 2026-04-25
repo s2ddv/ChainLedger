02012fb046a1cfc81ab2412aa68586b1acaca198
s2ddv <1speedftn@gmail.com>
2026-04-25 16:26:10 -0300

    fix: rename test connection

--- src/testConnection.ts
+++ src/testConnection.ts
@@ -3,7 +3,7 @@ import provider from './config/blockchain';
 
 async function main(): Promise<void> {
   const blockNumber = await provider.getBlockNumber();
-  console.log(`Spolia connected, current block:  ${blockNumber}`);
+  console.log(`Sepolia connected, current block:  ${blockNumber}`);
 
   const address = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045';
   const balance = await provider.getBalance(address);
